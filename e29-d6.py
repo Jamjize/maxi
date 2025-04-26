005c437ac2af5e93dcc056d297a9a231a83882fa
Jamjize <jamjizelol@gmail.com>
2025-04-26 00:30:34 -0300

    a

---
+++ e29-d6.py
@@ -0,0 +1,8 @@
+contador = 1
+while contador != 10: # mientras q contador no llegue a Dios
+    print(contador)
+    contador += 1
+    if contador == 5:
+        continue
+    if contador == 8:
+        break
