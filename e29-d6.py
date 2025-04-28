6c98f13cd4c99a1a034ec69df6adfe90911106ff
Jamjize <jamjizelol@gmail.com>
2025-04-27 22:45:23 -0300

    Concepto 18: Acumuladores y contadores

--- e29-d6.py
+++ e29-d6.py
@@ -1,8 +1,15 @@
-contador = 1
-while contador != 10: # mientras q contador no llegue a Dios
-    print(contador)
-    contador += 1
-    if contador == 5:
-        continue
-    if contador == 8:
+contador = 0
+clave = 0
+resultado = 0
+
+while True: # mientras q contador no llegue a Dios
+    intentos = int(input("ingresa el numero clave: "))
+
+    if clave != intentos:
+        contador += 1
+        print(contador)
+        resultado += intentos
+    else:
+        print(f"""Tus intentos son {contador} y 
+la suma de los numeros que pusistes es {resultado}""")
         break
