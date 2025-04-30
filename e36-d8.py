1455e24dc1fc7f4ed384f9a7fd92fa2c57a0e910
Jamjize <jamjizelol@gmail.com>
2025-04-30 20:46:43 -0300

    agregar al diccionario

--- e36-d8.py
+++ e36-d8.py
@@ -3,4 +3,4 @@ def agregar_nombre(lista):   # creaaaa la lista
     lista.append(nombres.lower())  # lista.append para agregar antes del nombre
     print(lista)  #  imprime la lista creada junto con el nombre
     
-agregar_nombre(["Maxi", "Gera"])  # La lista
+agregar_nombre(["Maxi", "Gera"])  # La mamos a la funcion
